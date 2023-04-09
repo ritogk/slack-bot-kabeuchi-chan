479abcf2ddfc16302965809eb91d11272ab4c34b
ryoji <ryoji@unko.com>
2023-04-09 18:11:46 +0900

    delete

--- app.ts
+++ app.ts
@@ -3,7 +3,6 @@ import dotenv from "dotenv"
 dotenv.config()
 
 // ボットトークンと Signing Secret を使ってアプリを初期化します
-// xapp-1-A05272W3J7Q-5067273469927-5d97da89a12f2c9b3226beb98d0b2f2b40816bb489fff1b2064279dcdd225eba
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -36,6 +35,7 @@ app.event("message", async ({ event, client }) => {
       "おっけー！壁打ちコーチングをするね。"
     )
   ) {
+    console.log(thread_ts)
     await client.chat.postMessage({
       channel: event.channel,
       thread_ts: thread_ts,
