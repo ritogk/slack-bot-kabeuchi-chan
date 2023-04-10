3120ec6c087be94782823cf3404d7bede391a8b9
ryoji <ryoji@unko.com>
2023-04-10 10:08:57 +0900

    回答制度をあげた

--- app.ts
+++ app.ts
@@ -14,12 +14,10 @@ const app = new App({
 // メンションにslackbotを指定して送信した場合のsubscribe
 app.event("app_mention", async ({ event, client, say, body }) => {
   // メッセージ内に「壁打」の文字列が含まれている。
-  if (event.text.includes("壁打")) {
-    await client.chat.postMessage({
-      channel: event.channel,
-      text: `はい！私が問いかけてあげる！\nこのスレッドに何か書き込んでね。`,
-    })
-  }
+  await client.chat.postMessage({
+    channel: event.channel,
+    text: `はい！私が壁打ち相手になってあげる！\nまずこのスレッドに解決したい課題を書いてね。`,
+  })
 })
 
 // メンションにslackbotを指定して送信した場合のsubscribe
@@ -31,31 +29,42 @@ app.event("message", async ({ event, client, body }) => {
     channel: event.channel,
     ts: thread_ts,
   })
+  // 壁打ちちゃんが生成したスレッドかどうかの判別
   if (
     parentMessage.messages !== undefined &&
     parentMessage.messages[0] &&
     parentMessage.messages[0].text?.includes(
-      `はい！私が問いかけてあげる！\nこのスレッドに何か書き込んでね。`
+      `はい！私が壁打ち相手になってあげる！\nまずこのスレッドに解決したい課題を書いてね。`
     )
   ) {
     // スレッドのやり取りを取得
     const storage = new SimpleStorage(`${thread_ts}.json`)
     const history = storage.get("history")
     const messages = history ? history : []
-    // スレッドのやり取りとコメントから返答を生成
-    messages.push({
-      role: "user",
-      content: `あなたは、プロの壁打ちコーチです。
-以下の制約条件と入力文をもとに、【出力内容】を出力してください。
+
+    const goal = parentMessage.messages[1].text
+    if (parentMessage.messages.length <= 2) {
+      messages.push({
+        role: "system",
+        content: `あなたはコーチングが得意なプロのカウンセラーです。
+以下の達成目標と制約条件と入力文をもとに回答してください。
+
+# 達成目標:
+${goal}
+
 # 制約条件：
 必ず質問をする事
 1文で答える事
 答えや意見を出さない事
-40文字以内で発言する事
+40文字以内で回答する事`,
+      })
+    } else {
+      messages.push({
+        role: "user",
+        content: text,
+      })
+    }
 
-# 入力文
-${text}`,
-    })
     // messages.push({ role: "user", content: text })
     const reply = await chatCompletion(messages)
     // スレッドのやり取りを保存
